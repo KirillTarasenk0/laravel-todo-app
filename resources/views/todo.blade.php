253367c6a2ff421c038e3de6adc98f2c22d5ffb4
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-02-05 02:19:54 +0300

    css folder with css files were created and linked to todo.blade.php

--- resources/views/todo.blade.php
+++ resources/views/todo.blade.php
@@ -6,8 +6,10 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>TODO Laravel app</title>
+    <link rel="stylesheet" href="{{ asset("css/reset.css") }}">
+    <link rel="stylesheet" href="{{ asset("css/styles.css") }}">
 </head>
 <body>
-    <h1>TODO Laravel app</h1>
+    <h1 class="title">TODO Laravel app</h1>
 </body>
 </html>
