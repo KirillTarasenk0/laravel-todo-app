00885509766de2e8ed7a2ce2915babc63d1c8e77
Kirill Tarasenko <ktarasenko003@gmail.com>
2024-06-07 02:37:43 +0300

    Skelethon of todo observer was made

---
+++ app/Observers/TodoObserver.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Observers;
+
+use App\Models\Task;
+
+class TodoObserver
+{
+    public function created(Task $task): void
+    {
+
+    }
+    public function updated(Task $task): void
+    {
+
+    }
+    public function deleted(Task $task): void
+    {
+
+    }
+
+    public function restored(Task $task): void
+    {
+
+    }
+    public function forceDeleted(Task $task): void
+    {
+
+    }
+}
